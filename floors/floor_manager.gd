41d69537c67ee7ae233d69bd33c977f3332f3e0f
AzzaamNasir <brovinciblesgaming@gmail.com>
2024-07-06 18:19:14 +0530

    Added Discord Rich Presence support

--- floors/floor_manager.gd
+++ floors/floor_manager.gd
@@ -3,3 +3,9 @@ extends Node2D
 @export var hatchery_options : Array
 @export var floor_map : Texture2D
 @export var characters : Array
+
+func _ready() -> void:
+	DiscordRPC.app_id = 1258685490937462826
+	DiscordRPC.details = "Godot Version"
+	DiscordRPC.large_image = "grassseal"
+	DiscordRPC.refresh()
